0662ae82cbf2e47e1ac5d3a5cba85430e03a9ce7
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-07-04 15:05:13 +0530

    MongoDB and Mongoose
    What and why Mongoose ?
    -Mongoose is a middleman between the actual database(mongodb) and the
    express controller, it has the schema, and according to it stores data
    
    - MongoDB will store literally anything, it does no know about
      schema,datatype associated with each table entry , or the actual table
      structure, it jut knows to store data, this creates a problem
    -Mongoose solves that problem , before storing data into mongoDB, it
    check is the data is what we have specified or not, it will check for
    datatype, table structure, and table definition before sending data into
    the MongoDB.

---
+++ express/backend/controller/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const studentScheme = new mongoose.Schema({
+    name:String,
+    semester: Number
+})
+const Student = mongoose.model("Student", studentScheme); 
