d187226f96a43847f702883b4509060c773a6cba
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-07-10 00:36:36 +0530

    controller debug

--- express/backend/controller/studentApiOneController.js
+++ express/backend/controller/studentApiOneController.js
@@ -1,7 +1,7 @@
 const { Student } = require("../model/Student");
 const getOne = async (req, res) => {
-  const student = await Student.find({ semester: req.query.semester });
-  if (!student) {
+  const students = await Student.find({ semester: req.query.semester });
+  if (!students) {//findOne is used to find only one instance of the field
     res.status(404).json({
       success: false,
       message: "not found",
@@ -10,7 +10,7 @@ const getOne = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "found",
-      data: student,
+      data: students,
     });
   }
 };
